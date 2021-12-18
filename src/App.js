41c5959c582f32473fd272cc672663b81b736d32
melihaycicek <aycckmlh@gmail.com>
2021-12-18 22:43:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useEffect, useState, } from 'react';
+import TodoItem from './components/todoItem';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [todoItems, setTodoItems] = useState(null);
+
+
+  useEffect(( ) => {
+    console.log("Hey WAHTSAUP");
+if(!todoItems){
+  fetch('http://localhost:8080/api/todoItems')
+  .then((response) => response.json())
+  .then((data)=>{
+    console.log("Todo items list:" , data);
+    setTodoItems(data);
+  }); 
+}
+  
+}, [todoItems]);
+
+
+
+function addNewTodoItem(){
+    
+fetch('http://localhost:8080/api/todoItems', {
+
+headers: {
+'content-type': 'application/json'
+
+},
+method:'POST',
+}).then(response => response.json())
+.then( (aTodoItem) => {
+ 
+  
+  setTodoItems ([...todoItems, aTodoItem]);
+});
+}
+    function handleDeleteTodoItem(item){
+     const updatedTodoItems =  todoItems.filter(
+       (aTodoItem) => aTodoItem.id !== item.id
+       );
+      setTodoItems([...updatedTodoItems]);
+    }
+  return ( 
+    <>
+    <div>
+    <button onClick={addNewTodoItem}>Add New Item </button>
+
     </div>
-  );
+    <div>
+     {todoItems 
+     ? todoItems.map((todoItem) => { 
+        return (  
+        <TodoItem 
+        key={todoItem.id} 
+        
+        data = {todoItem} 
+
+        emitDeleteTodoItem ={handleDelete0TodoItem}
+        />
+        );
+       })
+       :"loading data"}
+      </div>
+      </>
+   );
 }
 
 export default App;
